ff98ef042a32b296e2e7da8139a0443a14b90727
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-10 01:10:13 +0700

    config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,31 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot(),
+    AuthModule,
+    PostModule,
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Cors
   app.enableCors({
     origin: ['http://localhost:3000', 'https://starrick.site'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Lifeblog 2.0')
+    .setDescription('Lifeblog API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3030);
 }
 bootstrap();
