95d843ac88e1cf1065766d87e570137655a565ed
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-28 08:59:22 +0700

    add api tag

--- src/object-storage/object-storage.controller.ts
+++ src/object-storage/object-storage.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ObjectStorageService } from './object-storage.service';
 
 @Controller('object-storage')
+@ApiTags('object-storage')
 export class ObjectStorageController {
   constructor(private readonly objectStorageService: ObjectStorageService) {}
 
